6d58aed353c779ae4961e6855326b3542230727a
mvideet <mvideet@gmail.com>
2020-08-10 17:00:34 -0500

    test test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gitTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class gitTest extends OpMode {
-
+double x = 0.4;
     @Override
     public void init() {
 

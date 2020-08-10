e8ba02cd96096b5cbc0301703eae44fe1a82013a
mvideet <mvideet@gmail.com>
2020-08-10 17:02:35 -0500

    created 2 variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gitTest.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class gitTest extends OpMode {
 double x = 0.4;
+double y = 0.9;
+double z = 0.1;
     @Override
     public void init() {
 

b09db305685384534938d557cedc8cd2bdb10803
mvideet <mvideet@gmail.com>
2020-08-11 10:43:38 -0500

    Added a second motor to the motor config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newGitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newGitTest.java
@@ -6,19 +6,25 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class newGitTest extends LinearOpMode {
     DcMotor motor = null;
+    DcMotor newmotor = null;
     @Override
     public void runOpMode() throws InterruptedException {
         motor = hardwareMap.dcMotor.get("front_right");
+        newmotor = hardwareMap.dcMotor.get("front_left");
 
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        newmotor.setDirection(DcMotorSimple.Direction.REVERSE);
         motor.setPower(0);
+        newmotor.setPower(0);
         waitForStart();
         motor.setPower(0);
+        newmotor.setPower(0);
         while (opModeIsActive()){
             motor.setPower(1);
             delay(500);
             break;
         }
+
     }
     public void delay(double delayTime){
         double startTime = System.currentTimeMillis();

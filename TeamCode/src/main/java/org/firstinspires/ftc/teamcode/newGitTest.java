170543c5db51d6f14ea116d015b479441ca9ba18
RohanTD <rtdharia@gmail.com>
2020-08-11 10:45:57 -0500

    Editing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newGitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newGitTest.java
@@ -7,18 +7,31 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class newGitTest extends LinearOpMode {
     DcMotor motor = null;
     DcMotor newmotor = null;
+    DcMotor newermotor = null;
     @Override
     public void runOpMode() throws InterruptedException {
         motor = hardwareMap.dcMotor.get("front_right");
         newmotor = hardwareMap.dcMotor.get("front_left");
+        newermotor = hardwareMap.dcMotor.get("back_right");
 
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
         newmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        newermotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
         motor.setPower(0);
         newmotor.setPower(0);
+        newermotor.setPower(0);
+
+
         waitForStart();
+
+
         motor.setPower(0);
         newmotor.setPower(0);
+        newermotor.setPower(0);
+
+
         while (opModeIsActive()){
             motor.setPower(1);
             delay(500);

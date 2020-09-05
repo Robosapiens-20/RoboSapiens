5cffc025838018bf58195bfdf17cb0623e2d3921
mvideet <mvideet@gmail.com>
2020-09-05 13:15:23 -0500

    new version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/OdometryCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/OdometryCalibration.java
@@ -18,7 +18,7 @@ import java.io.File;
  * Odometry system calibration. Run this OpMode to generate the necessary constants to calculate the robot's global position on the field.
  * The Global Positioning Algorithm will not function and will throw an error if this program is not run first
  */
-@TeleOp(name = "Yeet", group = "Calibration")
+@TeleOp(name = "Yeet123", group = "Calibration")
 public class OdometryCalibration extends LinearOpMode {
     //Drive motors
     DcMotor right_front, right_back, left_front, left_back;

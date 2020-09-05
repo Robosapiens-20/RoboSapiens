4e2c5bc55a35e625a647b02420f44e9cdfdbbd3f
RohanTD <rtdharia@gmail.com>
2020-09-05 13:23:35 -0500

    Editing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
@@ -21,7 +21,7 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
     final double COUNTS_PER_INCH = 307.699557;
 
     //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
-    String verticalLeftEncoderName = "rf", verticalRightEncoderName = "lf", horizontalEncoderName = "lb";
+    String verticalLeftEncoderName = "front_right", verticalRightEncoderName = "front_left", horizontalEncoderName = "back_left";
 
     @Override
     public void runOpMode() throws InterruptedException {
